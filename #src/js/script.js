61c49f0a3e73556a1a319c1cccb55c661b734c39
Evgeny Groshev <webdev.yeisk@yandex.ru>
2020-08-03 18:18:54 +0300

    section banner-section

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,4 +1,10 @@
 $(function () {
 
 
+	$('.banner-section__slider').slick({
+		dots: true,
+		prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="images/arrows-left.svg" alt=""></button>',
+		nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="images/arrows-right.svg" alt=""></button>',
+	});
+
 });
--- gulpfile.js
+++ gulpfile.js
@@ -100,7 +100,7 @@ function style() {
 	return src([
 			/* Вставляем что нужно */
 			"node_modules/normalize.css/normalize.css",
-			"node_modules/slick-carousel/slick/slick-theme.css"
+			"node_modules/slick-carousel/slick/slick.css"
 		])
 		.pipe(concat("libs.min.css"))
 		.pipe(cssmin())
