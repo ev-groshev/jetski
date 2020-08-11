1374dec2bd3cb87b4eb184e063bbe3f42a90f852
Evgeny Groshev <webdev.yeisk@yandex.ru>
2020-08-11 11:27:44 +0300

    section products

--- #src/js/script.js
+++ #src/js/script.js
@@ -7,14 +7,25 @@ $(function () {
 		nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="images/arrows-right.svg" alt=""></button>',
 	});
 
-	$('.search__tabs-item').on('click', function (e) {
+	$('.tab').on('click', function (e) {
 		e.preventDefault();
 
-		$('.search__tabs-item').removeClass('search__tabs-item--active');
-		$('.search__content-item').removeClass('search__content-item--active');
+		$('.tab').removeClass('tab--active');
+		$('.tabs__content').removeClass('tabs__content--active');
 
-		$(this).addClass('search__tabs-item--active');
-		$($(this).attr('href')).addClass('search__content-item--active');
+		$(this).addClass('tab--active');
+		$($(this).attr('href')).addClass('tabs__content--active');
+	});
+
+	$('.product-item__favorite').on('click', function () {
+		$(this).toggleClass('product-item__favorite--active');
+	});
+
+	$('.product__slider').slick({
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		prevArrow: '<button class="product__slider-btn product__slider-btnprev"><img src="images/arrows-black-left.svg" alt=""></button>',
+		nextArrow: '<button class="product__slider-btn product__slider-btnnext"><img src="images/arrows-black-right.svg" alt=""></button>',
 	});
 
 });
